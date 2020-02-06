0e0a00a64075915266743b51473bebe853681237
habeezzy <mhabeebulah@gmail.com>
2020-02-06 16:30:04 -0500

    added simple validation

--- script.js
+++ script.js
@@ -19,17 +19,46 @@ function showSuccess(input) {
     formControl.className = 'form-control success';
 }
 
+
+//Check email is valid
+function validateEmail(email) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+
+
+
 //Event Listeners
 form.addEventListener('submit', function(e) {
     e.preventDefault();
 
     if (username.value === '') {
-        showError("username is required");
+        showError(username,"Username is required");
+    } else {
+        showSuccess(username);
+    }
+
+    if (email.value === '') {
+        showError(email, "Email is required");
+    } else if (!validateEmail(email.value)) {
+        showError(email, 'Email is not valid');
     }
     else {
-        showSuccess(username)
-    }
-    console.log("success")
+        showSuccess(email);
+    };
+
+    if (password.value === '') {
+        showError(password, "Password is required");
+    } else {
+        showSuccess(password);
+    };
+
+    if (password2.value === '') {
+        showError(password2, "Passwords do not match");
+    } else {
+        showSuccess(password2);
+    };
 });
 
 
